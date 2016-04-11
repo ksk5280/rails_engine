8da09d6fbd06c1125c64b858798552c10600bfd2
Kimiko Kano <kskano@gmail.com>
2016-04-11 17:09:30 -0600

    Create api/v1/merchants/:id endpoint

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -5,11 +5,23 @@ RSpec.describe "GET /api/v1/merchants" do
     merchant1 = Merchant.create(name: "Schroeder-Jerde")
     merchant2 = Merchant.create(name: "Klein, Rempel and Jones")
 
-    get '/api/v1/merchants'
+    get "/api/v1/merchants"
     json_body = JSON.parse(response.body)
 
     expect(response).to be_success
 
     expect(json_body.count).to eq(2)
+  end
+end
+
+RSpec.describe "GET /api/v1/merchants/1" do
+  it "returns a single merchant" do
+    merchant = Merchant.create(name: "Schroeder-Jerde")
+
+    get "/api/v1/merchants/#{merchant.id}"
+    json_body = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(json_body["name"]).to eq "Schroeder-Jerde"
   end
 end
